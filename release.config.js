8232e595359a9b5d8dc52f2e3628f759be5d5f6b
k3scat <k3scat@gmail.com>
2025-05-10 23:09:30 -0500

    chore: files cleanup

--- release.config.js
+++ release.config.js
@@ -4,17 +4,11 @@ module.exports = {
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     '@semantic-release/npm',
-    [
-      '@semantic-release/exec',
-      {
-        prepareCmd: 'node -e "const fs = require(\'fs\'); const pkg = JSON.parse(fs.readFileSync(\'./package.json\')); const jsr = JSON.parse(fs.readFileSync(\'./jsr.json\')); jsr.version = pkg.version; fs.writeFileSync(\'./jsr.json\', JSON.stringify(jsr, null, 4) + \'\\n\');"',
-      }
-    ],
     '@semantic-release/github',
     [
       '@semantic-release/git',
       {
-        assets: ['package.json', 'package-lock.json', 'jsr.json'],
+        assets: ['package.json', 'package-lock.json'],
         message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
       }
     ]
